928d6cf7adc48bb1c814a66cea6f37791afed70e
opilane <opilane@HP-CZC6297B8G>
2018-04-25 10:24:10 +0300

    add second greeting

--- JMgithub/Program.cs
+++ JMgithub/Program.cs
@@ -11,6 +11,7 @@ namespace JMgithub
         static void Main(string[] args)
         {
             Console.WriteLine("hello world!");
+            Console.WriteLine("hello univerce!");
         }
     }
 }
